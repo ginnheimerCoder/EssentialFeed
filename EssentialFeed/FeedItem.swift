2b0566b28e6abb5100f405412daf0fc3297ca8f6
David Gänshirt <david.gaenshirt@hr.de>
2023-08-25 11:45:54 +0200

    create the feed item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by David Gänshirt on 25.08.23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
