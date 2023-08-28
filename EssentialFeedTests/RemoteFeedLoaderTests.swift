caed781a11749785021e7ad81f425e3ba50e2c3c
David Gänshirt <david.gaenshirt@hr.de>
2023-08-28 15:13:27 +0200

    `RemoteFeedLoader` does not request data upon creation

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by David Gänshirt on 28.08.23.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        let sut = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+}
