889819c4f119466b72bb0c7e150fd8ee4943a52a
David Gänshirt <david.gaenshirt@hr.de>
2023-08-29 09:54:10 +0200

    Move `RemoteFeedLoader` components to production

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,29 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by David Gänshirt on 29.08.23.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
+
+
