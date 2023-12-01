e8bb9ae492b1439b42f2f7d7397c557016c36789
David Gänshirt <david.gaenshirt@hr.de>
2023-12-01 13:24:50 +0100

    CoreDataFeedStore.deleteCachedFeed delivers no error on empty cache

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -18,11 +18,10 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        
+        completion(nil)
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        
         let context = self.context
         context.perform {
             do {
