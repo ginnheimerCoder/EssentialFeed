2298d9b95da457c18bd5d3b69a2f5b2046c17d1c
David Gänshirt <david.gaenshirt@hr.de>
2023-12-04 09:44:43 +0100

    Add EssentialFeedCacheIntegrationTests target to seperate potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by David Gänshirt on 04.12.23.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+    
+
+}
