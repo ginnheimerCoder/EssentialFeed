f12abe3183f5b02b90c6b78d086d7e885e0f50df
David Gänshirt <david.gaenshirt@hr.de>
2024-02-22 15:25:06 +0100

    Set FeedViewController title

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -23,6 +23,8 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
         
     public override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "My Feed"
     }
     
     public override func viewIsAppearing(_ animated: Bool) {
