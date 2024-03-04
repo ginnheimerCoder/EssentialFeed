99a18020eb710658e2ca64efb68c5c43ed0275a8
David Gänshirt <david.gaenshirt@hr.de>
2024-03-04 14:37:12 +0100

    Move ErrorView to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by David Gänshirt on 04.03.24.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
