f558ef731527ebe417c4306bb4dad7f1d956cf66
David Gänshirt <david.gaenshirt@hr.de>
2024-03-04 15:09:35 +0100

    Move FeedErrorViewModel to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by David Gänshirt on 04.03.24.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
