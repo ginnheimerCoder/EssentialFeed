ac268661520a29a23a0df3514176a254a5a53a1e
David Gänshirt <david.gaenshirt@hr.de>
2024-06-14 11:11:35 +0200

    Implement Image Comments UI

--- EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -1,8 +1,36 @@
 //
-//  ImageCommentsController.swift
+//  ImageCommentCellController.swift
 //  EssentialFeediOS
 //
 //  Created by David Gänshirt on 14.06.24.
 //
 
-import Foundation
+import UIKit
+import EssentialFeed
+
+public class ImageCommentCellController: CellController {
+    
+    private let model: ImageCommentViewModel
+    
+    public init(model: ImageCommentViewModel) {
+        self.model = model
+    }
+    
+    public func view(in tableView: UITableView) -> UITableViewCell {
+        let cell: ImageCommentCell = tableView.dequeueReusableCell()
+        cell.messageLabel.text = model.message
+        cell.usernameLabel.text = model.username
+        cell.dateLabel.text = model.date
+        return cell
+    }
+    
+    public func preload() {
+        
+    }
+    
+    public func cancelLoad() {
+        
+    }
+    
+    
+}
---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by David Gänshirt on 14.06.24.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet public var messageLabel: UILabel!
+    @IBOutlet public var usernameLabel: UILabel!
+    @IBOutlet public var dateLabel: UILabel!
+}
