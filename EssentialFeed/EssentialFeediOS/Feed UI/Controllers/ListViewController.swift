473cfbc55ebba8a6e78f2983bed9d69ead4ee382
David Gänshirt <david.gaenshirt@hr.de>
2024-06-14 09:44:20 +0200

    Replace FeedViewControllerDelegate with a closure

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/ListViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/ListViewController.swift
@@ -8,10 +8,6 @@
 import UIKit
 import EssentialFeed
 
-public protocol FeedViewControllerDelegate {
-    func didRequestFeedRefresh()
-}
-
 public protocol CellController {
     func view(in tableView: UITableView) -> UITableViewCell
     func preload()
@@ -22,7 +18,7 @@ final public class ListViewController: UITableViewController, UITableViewDataSou
     
     private var viewAppeared = false
     
-    public var delegate: FeedViewControllerDelegate?
+    public var onRefresh: (() -> Void)?
     
     @IBOutlet private(set) public var errorView: ErrorView?
     
@@ -51,7 +47,7 @@ final public class ListViewController: UITableViewController, UITableViewDataSou
     }
     
     @IBAction private func refresh() {
-        delegate?.didRequestFeedRefresh()
+        onRefresh?()
     }
     
     public func display(_ cellControllers: [CellController]) {
