8b8d5e5a4473d125b6bff43e18763637012253af
David Gänshirt <david.gaenshirt@hr.de>
2024-06-14 10:23:04 +0200

    Extract shared ListViewController snapshot tests

--- EssentialFeed/EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
@@ -20,6 +20,15 @@ final class ListSnapshotTests: XCTestCase {
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_LIST_DARK")
     }
     
+    func test_listWithErrorMessage() {
+        let sut = makeSUT()
+        
+        sut.display(.error(message: "This is a\nmulti-line\nerror message"))
+        
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "LIST_WITH_ERROR_MESSAGE_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "LIST_WITH_ERROR_MESSAGE_dark")
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT() -> ListViewController {
