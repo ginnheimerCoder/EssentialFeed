0032e7f2c2b61baece2971d7e1391654aefd8ac8
David Gänshirt <david.gaenshirt@hr.de>
2024-06-17 08:30:49 +0200

    Add default implemenation for optional methods in the CellController protocol

--- EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -23,14 +23,5 @@ public class ImageCommentCellController: CellController {
         cell.dateLabel.text = model.date
         return cell
     }
-    
-    public func preload() {
-        
-    }
-    
-    public func cancelLoad() {
-        
-    }
-    
-    
+
 }
